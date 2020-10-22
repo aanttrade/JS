4013ca318aa184b5000b65c37148f76684ca1f64
Änton <aanttrade@gmail.com>
2020-10-22 21:12:37 +0300

    pervoe zanyatie

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-"use strict";
+/* Задание на урок:
 
-//alert('Hello');
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-//const result=confirm('Are you here?');
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-//console.log(result);
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-// const answer=prompt("Вам есть 18?","");
-// console.log(answer+5);
+Проверить, чтобы все работало без ошибок в консоли */
 
-// const answers = [];
+'use strict';
 
-// answers[0] = prompt("Как Ваше имя?","");
-// answers[1] = prompt("Как Ваша фамилия?","");
-// answers[2] = prompt("Сколько вам лет?","");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
-// document.write(answers);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const user=("Ivan");
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// alert(`Привет, ${user}`);
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
 
-// let incr = 10;
-//     decr = 10;
+      console.log(personalMovieDB);
+      
 
-//     // incr++;
-//     // decr--;
 
-//     console.log(incr++);
-//     console.log(decr--);
 
-const isChecked = false;
-      isClosed = false;
 
-      console.log(isChecked || !isClosed);
+
+
 
 
 
---
+++ script1.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//alert('Hello');
+
+//const result=confirm('Are you here?');
+
+//console.log(result);
+
+// const answer=prompt("Вам есть 18?","");
+// console.log(answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как Ваше имя?","");
+// answers[1] = prompt("Как Ваша фамилия?","");
+// answers[2] = prompt("Сколько вам лет?","");
+
+// document.write(answers);
+
+// const user=("Ivan");
+
+// alert(`Привет, ${user}`);
+
+// let incr = 10;
+//     decr = 10;
+
+//     // incr++;
+//     // decr--;
+
+//     console.log(incr++);
+//     console.log(decr--);
+
+const isChecked = false;
+      isClosed = false;
+
+      console.log(isChecked || !isClosed);
+
+
+
